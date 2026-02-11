46f0ca4af3bf06435a8d8ff9dbbc8eb0a0064582
vishnuvardhangude <vishnuvardhangude@gmail.com>
2026-02-11 14:05:59 +0800

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '132c90e981msh31bf9430db87c81p13e0f5jsn7c5b6fa62429',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
---
+++ src/components/search/Search.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
+import { GEO_API_URL, geoAPIOptions } from '../../api';
+
+const Search = ({onSearchChange}) => {
+
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  }
+
+  const loadOptions = async (inputValue) => {
+
+    
+
+    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions)
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+            options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            }
+          })
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for City"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+        
+  )
+}
+
+export default Search
