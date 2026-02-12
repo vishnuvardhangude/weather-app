4b127e87c76b1b72b713ca25ffb82e8880922c9e
vishnuvardhangude <vishnuvardhangude@gmail.com>
2026-02-12 17:24:16 +0800

    remove API keys

--- src/api.js
+++ src/api.js
@@ -2,12 +2,12 @@
 export const geoAPIOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': '132c90e981msh31bf9430db87c81p13e0f5jsn7c5b6fa62429',
+		'x-rapidapi-key': process.env.REACT_APP_GEO_API_KEY,
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&namePrefix=';
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "de7fd9068f6deca0100e431e44a68590";
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
